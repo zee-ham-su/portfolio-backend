4e7f53bb60e29508d51cc795e9ce6861d17f69c6
Hamza Sufian <hsufiian@yahoo.com>
2024-04-06 18:58:35 +0000

    app models

---
+++ Blog/models/comment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
