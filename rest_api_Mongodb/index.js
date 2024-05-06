d76b094950ab7eec0fa4e25c2e34c60f6033ce12
Hamza Sufian <hsufiian@yahoo.com>
2024-05-06 22:58:57 +0000

    chore: Update MongoDB connection and error handling in index.js

--- rest_api_Mongodb/index.js
+++ rest_api_Mongodb/index.js
@@ -5,6 +5,18 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+mongoose.connect('mongodb+srv://hamzasufian2014:hZ2j4i93ckRdHBrU@cluster0.ogwdwlq.mongodb.net/My_api')
+mongoose.connection.on('connected', () => {
+    console.log('DB connected');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+});
+mongoose.connection.on('error', (error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit process with failure
+});
+
 app.use(express.json());
 
 app.post('/', (req, res) => {
@@ -12,6 +24,3 @@ app.post('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
