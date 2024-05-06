7cb23032fb0e6a61bee267de37793919d7b4b75d
Hamza Sufian <hsufiian@yahoo.com>
2024-05-06 22:40:38 +0000

    chore: Add welcome message route to index.js

--- rest_api_Mongodb/index.js
+++ rest_api_Mongodb/index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json('Welcome to my REST API server!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
