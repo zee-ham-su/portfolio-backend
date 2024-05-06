aacdcc53ccfd77ea28065b55e2a969645eb2ba1c
Hamza Sufian <hsufiian@yahoo.com>
2024-05-06 23:22:00 +0000

    productmodel

---
+++ rest_api_Mongodb/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: String,
+    quantity: {
+        type: String,
+        default: 0
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['Electronics', 'Accessories', 'Books', 'Food']
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
