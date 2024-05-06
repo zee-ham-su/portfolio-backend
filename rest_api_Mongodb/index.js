73147de081f820e21d9164734835e10fdb750db2
Hamza Sufian <hsufiian@yahoo.com>
2024-05-06 22:43:49 +0000

    chore: Add welcome message route to index.js and update server.js and index.html

--- rest_api_Mongodb/index.js
+++ rest_api_Mongodb/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    console.log('Welcome to my REST API server!');
     res.json('Welcome to my REST API server!');
 });
 
