d25a0d020be72eac53f231073ef73c32fdb3ee58
Hamza Sufian <hsufiian@yahoo.com>
2024-05-06 22:38:04 +0000

    chore: Update main script in package.json to use nodemon

---
+++ rest_api_Mongodb/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
