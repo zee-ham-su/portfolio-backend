ff30fe0f7dc84f0a93268d227b229c833da407bf
Hamza Sufian <hsufiian@yahoo.com>
2024-05-07 20:09:35 +0000

    chore: Refactor REST API endpoints for better organization and modularity

---
+++ rest_api_Mongodb/controllers/productsController.js
@@ -0,0 +1,86 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
+exports.createProduct = async (req, res) => {
+    try {
+        if (!req.body.name) {
+            return res.status(400).json({ error: 'Name field is required' });
+        }
+
+        if (!req.body.price) {
+            return res.status(400).json({ error: 'price field is required' });
+        }
+
+        if (!req.body.category) {
+            return res.status(400).json({ error: 'category field is required' });
+        } else if (!Product.schema.path('category').enumValues.includes(req.body.category)) {
+            return res.status(400).json({ error: 'Invalid category' });
+        }
+        const newProduct = await Product.create(req.body)
+        return res.status(201).json(newProduct);
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+};
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find().select('-__v');
+        res.status(200).json(products);
+
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+};
+
+exports.getProductById = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(422).json({ error: 'Invalid product id' });
+        }
+        const product = await Product.findById(req.params.id).select('-__v');
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+};
+
+exports.updateProductById = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(422).json({ error: 'Invalid product id' });
+        }
+
+        if (!await Product.exists( {_id: req.params.id})) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true } );
+        
+        res.json(updatedProduct);
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.deleteProductById = async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(422).json({ error: 'Invalid product id' });
+        }
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        } else {
+            await Product.deleteOne()
+
+        return res.status(201).json({ message: 'Product deleted successfully' });
+        }
+        
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
