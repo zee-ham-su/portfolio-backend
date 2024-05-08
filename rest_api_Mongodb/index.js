a0ce80ddb5e3207ecd88863cc076cecf7c385387
Hamza Sufian <hsufiian@yahoo.com>
2024-05-08 14:52:58 +0000

    refactor: Update MongoDB connection URI in index.js

--- rest_api_Mongodb/index.js
+++ rest_api_Mongodb/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb+srv://hamzasufian2014:hZ2j4i93ckRdHBrU@cluster0.ogwdwlq.mongodb.net/My_api')
+mongoose.connect('uri')
 mongoose.connection.on('connected', () => {
     console.log('DB connected');
     app.listen(PORT, () => {
