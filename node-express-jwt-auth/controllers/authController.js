64f02d476c3b0b2c3334dc050e87764c7b54ba3e
Hamza Sufian <hsufiian@yahoo.com>
2024-05-09 19:35:46 +0000

    refactor: Implement user signup functionality in authController

--- node-express-jwt-auth/controllers/authController.js
+++ node-express-jwt-auth/controllers/authController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User');
+
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 };
@@ -6,13 +9,19 @@ module.exports.login_get = (req, res) => {
     res.render('login');
 };
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
-    console.log(req.body);
-    res.send('new signup');
+    try {
+        const user = await User.create({ email: email, password: password});
+        res.status(201).json(user);
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ error: "user is not created" });
+    }
 };
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
     console.log(req.body); 
