b5b0b6d60addd438a94dc76bb8fa5cecda402345
Hamza Sufian <hsufiian@yahoo.com>
2024-05-09 14:13:52 +0000

    refactor: Update authRoutes to use controller functions for signup and login

--- node-express-jwt-auth/routes/authRoutes.js
+++ node-express-jwt-auth/routes/authRoutes.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 const router = Router
 const authController = require('../controllers/authController');
 
-router.get('/signup', () => {});
-router.post('/signup', () => {});
-router.get('/login', () => {});
-router.post('/login', () => {});
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
+router.get('/login', authController.login_get);
+router.post('/login', authController.login_post);
 
 module.exports = router;
