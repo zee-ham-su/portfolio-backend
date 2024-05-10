88b447d75f699d923c2878ff84c384ba424a339a
Hamza Sufian <hsufiian@yahoo.com>
2024-05-10 21:14:54 +0000

    refactor: Handle duplicate email error in authController.js

--- node-express-jwt-auth/controllers/authController.js
+++ node-express-jwt-auth/controllers/authController.js
@@ -4,6 +4,13 @@ const User = require('../models/User');
 const handleErrors = (err) => {
     console.error(err.message, err.code);
     let errors = { email: '', password: '' };
+
+    // duplicate error code 
+    if (err.code === 11000) {
+        errors.email = 'That email is already registered';
+        return errors;
+    }
+    
     // validation errors
     if (err.message.includes('user validation failed')) {
         Object.values(err.errors).forEach(({ properties }) => {
