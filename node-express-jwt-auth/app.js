38bdce94b20f59f4c467ea410c24d31733d29eb4
Hamza Sufian <hsufiian@yahoo.com>
2024-05-10 18:58:29 +0000

    refactor: Update MongoDB connection URI and add error handling in app.js

--- node-express-jwt-auth/app.js
+++ node-express-jwt-auth/app.js
@@ -1,51 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const uri = "mongodb+srv://hamzasufian2014:hZ2j4i93ckRdHBrU@cluster0.ogwdwlq.mongodb.net/auth_node";
 
-// middleware
+// Middleware
 app.use(express.static('public'));
 app.use(express.json());
 
-// view engine
+// View engine
 app.set('view engine', 'ejs');
 
-// database connection
+// Database connection
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Database connection error:', err.message);
+    // Handle error appropriately, e.g., retry connection or terminate the application
+  });
 
-
-const uri = "mongodb+srv://hamzasufian2014:hZ2j4i93ckRdHBrU@cluster0.ogwdwlq.mongodb.net/";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-app.listen(3000, () => {
-  console.log('server started on http://localhost:3000');
-});
-
-// routes
+// Routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 
