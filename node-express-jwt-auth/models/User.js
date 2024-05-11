20be71eab910cf4921fc8c41b8021a05a9142256
Hamza Sufian <hsufiian@yahoo.com>
2024-05-11 17:46:41 +0000

    refactor: Update pre-save hook in User model to use bcrypt for password hashing

--- node-express-jwt-auth/models/User.js
+++ node-express-jwt-auth/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -28,7 +29,9 @@ userSchema.post('save', function(doc, next) {
 });
 
 // fire a function before doc saved to db
-userSchema.pre('save', function(next) {
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password  = await bcrypt.hash(this.password, salt);
     console.log('user about to be created & saved', this);
     next();
 });
