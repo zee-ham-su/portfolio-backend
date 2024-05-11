2c49f460742b3433842a3054967c32bbd76582dd
Hamza Sufian <hsufiian@yahoo.com>
2024-05-11 17:28:55 +0000

    refactor: Enable saving new user without console log in User model post save hook

--- node-express-jwt-auth/models/User.js
+++ node-express-jwt-auth/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.post('save', function(doc, next) {
     console.log('new user was created & saved', doc);
-    //next();
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
