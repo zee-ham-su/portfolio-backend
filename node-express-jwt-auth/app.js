c45214fd93fc01785268209f41d72201b0c1c1da
Hamza Sufian <hsufiian@yahoo.com>
2024-05-13 14:20:23 +0000

    refactor: Update cookie handling in app.js

--- node-express-jwt-auth/app.js
+++ node-express-jwt-auth/app.js
@@ -37,18 +37,3 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use('/api', authRoutes);
 
 
-// cookies
-
-app.get('/set-cookies', (req, res) => {
-  
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
-  res.send('you got the cookie!')
-});
-
-app.get('/read-cookies', (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies);
-  res.send(cookies);
-
-});
