e3ed413e0daead5c5d9d804105a50781fb445281
Hamza Sufian <hsufiian@yahoo.com>
2024-05-17 20:25:36 +0000

    refactor: Add login functionality to login_post method in authController.js

--- node-express-jwt-auth/controllers/authController.js
+++ node-express-jwt-auth/controllers/authController.js
@@ -51,10 +51,13 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
+    try {
+        const user = await User.login(email, password);
+        res.status(200).json({ user: user._id });
+    }
+    
+    catch (err) {
+        res.status(400).json({ errors });
 
-    User.login(email, password)
-
-    console.log(req.body); 
-    res.send('user login');
+}
 };
-
